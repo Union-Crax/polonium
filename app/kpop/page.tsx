cde8ce31b61e5c44a71a7b22fbc7f7649b8ff0f7
Nugget <Union-crax@proton.me>
2025-07-08 20:28:13 +0200

    Add KpopPage component with embedded video

---
+++ app/kpop/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const KpopPage = () => {
+  return (
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="https://www.2embed.cc/embed/tt14205554"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+        style={{ display: "block", border: 0, width: "100%", height: "100vh" }}
+        title="Kpop Video"
+      />
+    </div>
+  );
+};
+
+export default KpopPage;
